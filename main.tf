f0ce090dd439ae0a5101639ce6b0c04045caf4ec
Ben Pilgrim <ben@pilgrim.me.uk>
2021-05-28 19:33:47 +0100

    Multi AZ DMZ

--- main.tf
+++ main.tf
@@ -40,16 +40,6 @@ resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
 }
 
-resource "aws_subnet" "dmz-eu-west-1a" {
-  cidr_block              = "10.0.0.0/24"
-  vpc_id                  = aws_vpc.main.id
-  map_public_ip_on_launch = true
-  availability_zone       = "eu-west-1a"
-  tags = {
-    Name = "dmz-eu-west-1a"
-  }
-}
-
 resource "aws_route_table" "dmz" {
   vpc_id = aws_vpc.main.id
 
@@ -59,42 +49,78 @@ resource "aws_route_table" "dmz" {
   }
 }
 
-resource "aws_route_table_association" "dmz-eu-west-1a" {
-  route_table_id = aws_route_table.dmz.id
-  subnet_id = aws_subnet.dmz-eu-west-1a.id
+variable "availability_zones" {
+  type = list(object({
+    name: string
+    id: string
+    cidr: number
+  }))
+  default = [{
+    name: "eu-west-1a"
+    id: "euw1-az1"
+  },
+  {
+    name: "eu-west-1b"
+    id: "euw1-az2"
+  }]
 }
 
-resource "aws_subnet" "app-eu-west-1a" {
-  cidr_block              = "10.0.1.0/24"
+// DMZ EUW1 AZ1
+
+resource "aws_subnet" "dmz-subnets" {
+  count = length(var.availability_zones)
+
+  cidr_block              = "10.0.${count.index}.0/24"
+  vpc_id                  = aws_vpc.main.id
+  map_public_ip_on_launch = true
+  availability_zone       = lookup(var.availability_zones[count.index], "name")
+  tags = {
+    Name = "dmz-${lookup(var.availability_zones[count.index], "id")}"
+  }
+}
+
+resource "aws_route_table_association" "dmz-eu-west-1a" {
+  count = length(var.availability_zones)
+
+  route_table_id = aws_route_table.dmz.id
+  subnet_id = aws_subnet.dmz-subnets[count.index].id
+}
+
+// APP EUW1 AZ1
+
+resource "aws_subnet" "app-euw1-az1" {
+  cidr_block              = "10.0.100.0/24"
   vpc_id                  = aws_vpc.main.id
   availability_zone       = "eu-west-1a"
   tags = {
-    Name = "app-eu-west-1a"
+    Name = "app-euw1-az1"
   }
 }
 
-resource "aws_eip" "app-eu-west-1a-nat" {
-
-}
-
-resource "aws_nat_gateway" "app-eu-west-1a" {
-  allocation_id = aws_eip.app-eu-west-1a-nat.id
-  subnet_id = aws_subnet.app-eu-west-1a.id
+resource "aws_eip" "app-euw1-az1-nat" {
   tags = {
-    Name = "app-eu-west-1a"
+    Name = "app-euw1-az1-nat"
   }
 }
 
-resource "aws_route_table" "app" {
+resource "aws_nat_gateway" "app-euw1-az1" {
+  allocation_id = aws_eip.app-euw1-az1-nat.id
+  subnet_id = aws_subnet.app-euw1-az1.id
+  tags = {
+    Name = "app-euw1-az1"
+  }
+}
+
+resource "aws_route_table" "app-euw1-az1" {
   vpc_id = aws_vpc.main.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.app-eu-west-1a.id
+    nat_gateway_id = aws_nat_gateway.app-euw1-az1.id
   }
 }
 
 resource "aws_route_table_association" "app-eu-west-1a" {
-  route_table_id = aws_route_table.app.id
-  subnet_id = aws_subnet.app-eu-west-1a.id
+  route_table_id = aws_route_table.app-euw1-az1.id
+  subnet_id = aws_subnet.app-euw1-az1.id
 }
