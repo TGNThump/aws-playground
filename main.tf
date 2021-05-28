2e6a99d86675d0740cdafedf95679202be271c90
Ben Pilgrim <ben@pilgrim.me.uk>
2021-05-28 21:36:10 +0100

    App Subnet per AZ

--- main.tf
+++ main.tf
@@ -64,7 +64,7 @@ variable "availability_zones" {
   }]
 }
 
-// DMZ EUW1 AZ1
+// DMZ
 
 resource "aws_subnet" "dmz-subnets" {
   count = length(var.availability_zones)
@@ -85,41 +85,50 @@ resource "aws_route_table_association" "dmz-route-table-associations" {
   subnet_id = aws_subnet.dmz-subnets[count.index].id
 }
 
-// APP EUW1 AZ1
+// APP
 
-resource "aws_subnet" "app-euw1-az1" {
-  cidr_block              = "10.0.100.0/24"
+resource "aws_subnet" "app-subnets" {
+  count = length(var.availability_zones)
+
+  cidr_block              = "10.0.${10 + count.index}.0/24"
   vpc_id                  = aws_vpc.main.id
-  availability_zone       = "eu-west-1a"
+  availability_zone       = var.availability_zones[count.index].name
   tags = {
-    Name = "app-euw1-az1"
+    Name = "app-${var.availability_zones[count.index].id}"
   }
 }
 
-resource "aws_eip" "app-euw1-az1-nat" {
+resource "aws_eip" "app-nat-eips" {
+  count = length(var.availability_zones)
+
   tags = {
-    Name = "app-euw1-az1-nat"
+    Name = "app-${var.availability_zones[count.index].id}-nat"
   }
 }
 
-resource "aws_nat_gateway" "app-euw1-az1" {
-  allocation_id = aws_eip.app-euw1-az1-nat.id
-  subnet_id = aws_subnet.app-euw1-az1.id
+resource "aws_nat_gateway" "app-nat-gateways" {
+  count = length(var.availability_zones)
+
+  allocation_id = aws_eip.app-nat-eips[count.index].id
+  subnet_id = aws_subnet.app-subnets[count.index].id
   tags = {
-    Name = "app-euw1-az1"
+    Name = "app-${var.availability_zones[count.index].id}-nat"
   }
 }
 
-resource "aws_route_table" "app-euw1-az1" {
+resource "aws_route_table" "app-route-tables" {
+  count = length(var.availability_zones)
   vpc_id = aws_vpc.main.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.app-euw1-az1.id
+    nat_gateway_id = aws_nat_gateway.app-nat-gateways[count.index].id
   }
 }
 
-resource "aws_route_table_association" "app-eu-west-1a" {
-  route_table_id = aws_route_table.app-euw1-az1.id
-  subnet_id = aws_subnet.app-euw1-az1.id
+resource "aws_route_table_association" "app-route-table-associations" {
+  count = length(var.availability_zones)
+
+  route_table_id = aws_route_table.app-route-tables[count.index].id
+  subnet_id = aws_subnet.app-subnets[count.index].id
 }
