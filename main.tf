f7e8e98b3fbcc2623af29034b92ea7ba3f68a16f
Ben Pilgrim <ben@pilgrim.me.uk>
2021-05-29 17:33:39 +0100

    Point DNS to CloudFront

--- main.tf
+++ main.tf
@@ -284,8 +284,8 @@ resource "aws_route53_record" "dmz-lb-a" {
   zone_id = aws_route53_zone.main.id
   alias {
     evaluate_target_health = false
-    name = aws_alb.dmz-lb.dns_name
-    zone_id = aws_alb.dmz-lb.zone_id
+    name = aws_cloudfront_distribution.main.domain_name
+    zone_id = aws_cloudfront_distribution.main.hosted_zone_id
   }
 }
 
