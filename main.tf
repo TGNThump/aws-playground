dcd90c972d42e44bb36fbae76ccf8f88df8c34b2
Ben Pilgrim <ben@pilgrim.me.uk>
2021-05-29 14:02:21 +0100

    Add ECS Image

--- main.tf
+++ main.tf
@@ -161,6 +161,27 @@ resource "aws_security_group" "app" {
     to_port = 80
     security_groups = [aws_security_group.albs.id]
   }
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 65535
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
 }
 
 data "cloudflare_zones" "main" {
@@ -285,13 +306,8 @@ resource "aws_alb_listener" "https" {
   protocol = "HTTPS"
   certificate_arn = aws_acm_certificate.cert.arn
   default_action {
-    type = "fixed-response"
-
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "Yay!"
-      status_code = "200"
-    }
+    type = "forward"
+    target_group_arn = aws_alb_target_group.test_service.arn
   }
 }
 
@@ -301,4 +317,77 @@ resource "aws_ecs_cluster" "main" {
   default_capacity_provider_strategy {
     capacity_provider = "FARGATE_SPOT"
   }
+}
+
+
+resource "aws_alb_target_group" "test_service" {
+  name = "test_service"
+  port = 80
+  protocol = "HTTP"
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_ecs_task_definition" "test_task_definition" {
+  family = "test_task_definition"
+  container_definitions = jsonencode([
+    {
+      name = "hello_word"
+      image = "tutum/hello-world"
+      cpu = 2
+      memory = 512
+      essential = true
+      portMappings = [
+        {
+          containerPort: 80
+          hostPort: 80
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_service" "test_service" {
+  name = "test_service"
+  cluster = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.test_task_definition.arn
+  desired_count = 2
+  launch_type = "FARGATE"
+  iam_role        = aws_iam_role.ecs_agent.arn
+
+  load_balancer {
+    target_group_arn = aws_alb_target_group.test_service.arn
+    container_name = "hello_world"
+    container_port = 0
+  }
+
+  network_configuration {
+    subnets = [aws_subnet.app-subnets.*.id]
+    security_groups = [aws_security_group.app.id]
+  }
+}
+
+data "aws_iam_policy_document" "ecs_agent" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ecs_agent" {
+  name               = "ecs-agent"
+  assume_role_policy = data.aws_iam_policy_document.ecs_agent.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_agent" {
+  role       = "aws_iam_role.ecs_agent.name"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
+resource "aws_iam_instance_profile" "ecs_agent" {
+  name = "ecs-agent"
+  role = aws_iam_role.ecs_agent.name
 }
