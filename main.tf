84969b24582b5593b1a07c9daa8997ca5e6ffdb5
Ben Pilgrim <ben@pilgrim.me.uk>
2021-05-29 17:57:18 +0100

    Validate certs in two regions

--- main.tf
+++ main.tf
@@ -261,6 +261,29 @@ resource "aws_acm_certificate" "cert" {
   }
 }
 
+//noinspection HILUnresolvedReference
+resource "aws_route53_record" "domain-validation-records" {
+  for_each = {
+  for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+    name   = dvo.resource_record_name
+    record = dvo.resource_record_value
+    type   = dvo.resource_record_type
+  }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.main.zone_id
+}
+
+resource "aws_acm_certificate_validation" "validation" {
+  certificate_arn = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.domain-validation-records : record.fqdn]
+}
+
 resource "aws_acm_certificate" "cert-us-east-1" {
   provider = aws.us-east-1
   domain_name = "aws.pilgrim.me.uk"
@@ -278,11 +301,11 @@ resource "aws_acm_certificate" "cert-us-east-1" {
 //noinspection HILUnresolvedReference
 resource "aws_route53_record" "domain-validation-records" {
   for_each = {
-    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-   }
+  for dvo in aws_acm_certificate.cert-us-east-1.domain_validation_options : dvo.domain_name => {
+    name   = dvo.resource_record_name
+    record = dvo.resource_record_value
+    type   = dvo.resource_record_type
+  }
   }
 
   allow_overwrite = true
@@ -294,7 +317,7 @@ resource "aws_route53_record" "domain-validation-records" {
 }
 
 resource "aws_acm_certificate_validation" "validation" {
-  certificate_arn = aws_acm_certificate.cert.arn
+  certificate_arn = aws_acm_certificate.cert-us-east-1.arn
   validation_record_fqdns = [for record in aws_route53_record.domain-validation-records : record.fqdn]
 }
 
